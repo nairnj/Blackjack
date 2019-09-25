0b0748c62b84fdc8460704f7ff00256fe1bebaac
John A. Nairn <john@geditcom.com>
2019-09-25 00:13:28 -0700

    Update Mac project and makefile
    Revise XCode project to  work with code that was edited to work in Visual Studio. Also updated makefile to work with Visual Studio changes.

---
+++ source/Headers/stdafx.h
@@ -0,0 +1,13 @@
+/********************************************************************************
+	stdafx.h
+	Blackjack
+
+	Created by John Nairn on 09/24/2019
+	Copyright (c) 2019 John A. Nairn, All rights reserved.
+********************************************************************************/
+
+// This empty file is used to make Mac and Linux code compatible with
+// Visual Studio code.
+
+// To compile the full path to the folder containing this file must be
+// in the search paths for included headers.
